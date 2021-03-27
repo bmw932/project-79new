f71058e15aa3b21c64d0b79f89771e706a1d44ec
bmw932 <77596510+bmw932@users.noreply.github.com>
2021-03-27 11:05:08 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+array_of_pizzas=["Greek Pizza","Hawaiian Pizza", "New York Style", "Chicago-Style Pizza", "Californie-Style Pizza", "Pepperoni"];
+function getmenu(){
+   var menulist;
+   menulist="<ol class='menulist'>";
+   array_of_pizzas.sort();
+   for(i=0;i<array_of_pizzas.length;i++){
+       menulist=menulist+'<li>'+array_of_pizzas[i]+'</li>';
+   }
+   menulist=menulist+'</ol>';
+   document.getElementById("display_menu").innerHTML=menulist;
+}
+function add_item(){
+    var pizza=document.getElementById("add_item").value;
+    array_of_pizzas.push(pizza);
+    array_of_pizzas.sort();
+    var sections;
+    sections="<section class='cards'>";
+    for(i=0;i<array_of_pizzas.length;i++){
+        sections=sections+'<div id="cards">'+'<img src="images/pizzaimg.png"/>'+'</div>';
+    }
+ sections=sections+'</section>';
+ document.getElementById("display_addedmenu").innerHTML=sections;   
+}
